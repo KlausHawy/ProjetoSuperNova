7f2a78ad2159635acbeee41297b388c5680bd312
Quéfrem Lourran <7lourran@gmail.com>
2023-10-16 21:53:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ProjetoSuperNova
+ ProjetoSuperNova
---
+++ paginas/js.js
@@ -0,0 +1,9 @@
+var detalhes = document.getElementById("action-button_dtl");
+
+detalhes.addEventListener("click", function(){
+
+    var dtl = document.getElementById("detalhes1");
+
+    dtl.classList.toggle("hide");
+    
+});
